0e33f66398d3b86154e8f7dc5adb2b02f56351e6
Rishabh9742 <rairishabhrai9742@gmail.com>
2026-03-12 12:20:49 +0530

    Added category filters
    Improved the website by added more css and category filters

--- estore/src/App.js
+++ estore/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
+import ProtectedRoute from "./components/ProtectedRoute";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import ProductDetails from "./pages/ProductDetails";
@@ -24,7 +24,14 @@ function App() {
 
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/checkout" element={<Checkout />} />
+          <Route
+      path="/checkout"
+      element={
+        <ProtectedRoute>
+          <Checkout />
+        </ProtectedRoute>
+      }
+    />
       </Routes>
 
       <Footer />
--- estore/src/pages/Home.js
+++ estore/src/pages/Home.js
@@ -7,39 +7,84 @@ function Home() {
 
   const [products, setProducts] = useState([]);
   const [search, setSearch] = useState("");
+  const [category, setCategory] = useState("all");
 
   useEffect(() => {
     getProducts().then(setProducts);
   }, []);
 
-  const filtered = products.filter(p =>
-    p.title.toLowerCase().includes(
-      search.toLowerCase()
-    )
-  );
+  const categories = [
+    "all",
+    ...new Set(products.map(p => p.category))
+  ];
+
+  const filtered = products.filter(p => {
+
+    const matchSearch =
+      p.title
+        .toLowerCase()
+        .includes(search.toLowerCase());
+
+    const matchCategory =
+      category === "all" ||
+      p.category === category;
+
+    return matchSearch && matchCategory;
+
+  });
 
   return (
-    <div>
 
-      <SearchBar setSearch={setSearch} />
+    <div className="home-layout">
 
-      <div className="container">
+      {/* sidebar */}
 
-        {filtered.map(p => (
-          <ProductCard
-            key={p.id}
-            product={{
-              id: p.id,
-              name: p.title,
-              price: p.price,
-              image: p.image
-            }}
-          />
+      <div className="sidebar">
+
+        <h3>Categories</h3>
+
+        {categories.map(c => (
+
+          <button
+            key={c}
+            onClick={() => setCategory(c)}
+          >
+            {c}
+          </button>
+
         ))}
 
       </div>
 
+
+      {/* main */}
+
+      <div className="main">
+
+        <SearchBar setSearch={setSearch} />
+
+        <div className="container">
+
+          {filtered.map(p => (
+
+            <ProductCard
+              key={p.id}
+              product={{
+                id: p.id,
+                name: p.title,
+                price: p.price,
+                image: p.image
+              }}
+            />
+
+          ))}
+
+        </div>
+
+      </div>
+
     </div>
+
   );
 }
 
